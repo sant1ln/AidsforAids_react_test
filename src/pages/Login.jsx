acc932978533157f620136d2a20e0036c6eee564
Santiag Lopera <santiagoln09@gmail.com>
2022-05-27 11:24:24 -0500

    [Feature] Login view created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/Login.css'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <section className='login'>
+      <h1>Login</h1>
+      <form className='login_form'>
+        <label htmlFor="email" className='login_input'>
+          <span>Email</span>
+          <input type="text" name="email" />
+        </label>
+
+        <label htmlFor="password" className='login_input'>
+          <span>Password</span>
+          <input type="password" name="password" />
+        </label>
+
+        <button className='login_submit'>Login</button>
+        <Link className='invite'  to="/signin">don't have an account? sign up here</Link>
+      </form>
+
+    </section>
   )
 }
