cb0a9b84b24f615d2324e96a7fdfec65631f7309
Santiag Lopera <santiagoln09@gmail.com>
2022-05-27 11:43:10 -0500

    [Feature] Banner for sigin view

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import '../styles/Signin.css'
+import bannerImage from '../assets/signin_invite.svg'
 
 export const Signin = () => {
   return (
-    <div>Signin</div>
+    <section className='signin'>
+      <section className='signin_banner'>
+          <h2>
+            The Little Store  
+          </h2>
+          <p>Where you can buy that what you've been dreaming of</p>
+
+          <img src={bannerImage} alt="Banner image" />
+      </section>
+      <section className='signin_form'></section>
+    </section>
   )
 }
