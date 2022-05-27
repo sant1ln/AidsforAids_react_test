f4107a7a4408e4c3c3fcad074a034066e65435a4
Santiag Lopera <santiagoln09@gmail.com>
2022-05-27 11:58:21 -0500

    [Feature] SignIn view done

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Signin.css'
 import bannerImage from '../assets/signin_invite.svg'
+import { Link } from 'react-router-dom'
 
 export const Signin = () => {
   return (
-    <section className='signin'>
+    <section className='signin_container'>
       <section className='signin_banner'>
           <h2>
             The Little Store  
@@ -13,7 +14,36 @@ export const Signin = () => {
 
           <img src={bannerImage} alt="Banner image" />
       </section>
-      <section className='signin_form'></section>
+      <section className='signin'>
+        <h2>Sign in!</h2>
+      <form className='signin_form'>
+
+        <label htmlFor="name" className='signin_input'>
+          <span>Name</span>
+          <input type="text" name="name" />
+        </label>
+
+
+        <label htmlFor="email" className='signin_input'>
+          <span>Email</span>
+          <input type="text" name="email" />
+        </label>
+
+        <label htmlFor="password" className='signin_input'>
+          <span>Password</span>
+          <input type="password" name="password" />
+        </label>
+
+        <label htmlFor="password_check" className='signin_input'>
+          <span>Check Password</span>
+          <input type="password" name="password_check" />
+        </label>
+
+        <button className='signin_submit'>Signin</button>
+        <Link className='invite'  to="/signin">Do you already have an account? log in here</Link>
+      </form>
+
+      </section>
     </section>
   )
 }
